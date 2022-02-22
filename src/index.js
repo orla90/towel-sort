5cd2be14c59bb82c7fa7a16adf3ca690b2101ef6
orla90 <orla90@inbox.ru>
2022-02-22 23:42:19 +0300

    fix: fix one test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 
 module.exports = function towelSort (matrix) {
   let result = [];
-  if (matrix.length > 0) {
+  if (matrix) {
     for (let i = 0; i < matrix.length; i++) {
       for (let j = 0; j < matrix[i].length; j++) {
         let matrixItem = (i % 2) ? matrix[i].length - 1 - j : j;
